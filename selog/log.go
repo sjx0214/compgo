dc6148293fe0e40ee04ac4bdfab53bbd68791561
Stephen Ji <2849413550@qq.com>
2023-01-04 21:27:40 +0800

    初始搭建log雏形

--- selog/log.go
+++ selog/log.go
@@ -1,7 +1,64 @@
 package selog
 
-import "fmt"
+import (
+	"os"
+	"path"
+	"runtime"
 
-func Tuse(name string) {
-	fmt.Println(name)
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var logger *zap.Logger
+
+func init() {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	// 设置日志记录中时间的格式
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	// 日志Encoder 还是JSONEncoder，把日志行格式化成JSON格式的
+	encoder := zapcore.NewConsoleEncoder(encoderConfig)
+	file, err := os.OpenFile("../logs/test.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fileWriteSyncer := zapcore.AddSync(file)
+	core := zapcore.NewTee(
+		// 同时向控制台和文件写日志， 生产环境记得把控制台写入去掉，日志记录的基本是Debug 及以上，生产环境记得改成Info
+		zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), zapcore.DebugLevel),
+		zapcore.NewCore(encoder, fileWriteSyncer, zapcore.DebugLevel),
+	)
+	logger = zap.New(core)
+}
+
+func Info(message string, fields ...zap.Field) {
+	callerFields := getCallerInfoForLog()
+	fields = append(fields, callerFields...)
+	logger.Info(message, fields...)
+}
+func Debug(message string, fields ...zap.Field) {
+	callerFields := getCallerInfoForLog()
+	fields = append(fields, callerFields...)
+	logger.Debug(message, fields...)
+}
+func Error(message string, fields ...zap.Field) {
+	callerFields := getCallerInfoForLog()
+	fields = append(fields, callerFields...)
+	logger.Error(message, fields...)
+}
+func Warn(message string, fields ...zap.Field) {
+	callerFields := getCallerInfoForLog()
+	fields = append(fields, callerFields...)
+	logger.Warn(message, fields...)
+}
+func getCallerInfoForLog() (callerFields []zap.Field) {
+	pc, file, line, ok := runtime.Caller(2) // 回溯两层，拿到写日志的调用方的函数信息
+	if !ok {
+		return
+	}
+
+	funcName := runtime.FuncForPC(pc).Name()
+	funcName = path.Base(funcName) //Base函数返回路径的最后一个元素，只保留函数名
+	fileName := path.Base(file)
+	callerFields = append(callerFields, zap.String("func", funcName), zap.String("file", fileName), zap.Int("line", line))
+	return
 }
